7554ffa98301a5e8e97b3bb686cf1f3ddbf38fce
Revenko Vladislav <revad8@gmail.com>
2023-03-02 23:42:12 +0300

    Добавьте файлы проекта.

---
+++ Task 13.6.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace Task_13._6._1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<char> list = new List<char>();
+            LinkedList<char> linked = new LinkedList<char>();
+
+            Console.Write("Введите путь к файлу: ");
+            var text = GetText(Console.ReadLine());
+
+            var speedList = Stopwatch.StartNew();
+            foreach (char c in text)
+            {
+                list.Add(c);
+            }
+            Console.WriteLine($"List отработал за {speedList.Elapsed.TotalMilliseconds} мс. Вставленно {text.Count()} символов.");
+
+            var speedLinked = Stopwatch.StartNew();
+            foreach (char c in text)
+            {
+                linked.AddLast(c);
+            }
+
+            Console.WriteLine($"LinkedList отработал за {speedList.Elapsed.TotalMilliseconds} мс. Вставленно {text.Count()} символов.");
+        }
+
+        static string GetText(string path)
+        {
+            string text;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            return text;
+        }
+
+    }
+
+}
---
+++ Task 13.6.2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+namespace Task_13._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к файлу: ");
+
+            var text = GetText(Console.ReadLine());
+            Dictionary<String, Int32> wordsCount = new Dictionary<String, Int32>();
+
+            foreach (var word in text)
+            {
+                if (wordsCount.ContainsKey(word))
+                {
+                    wordsCount[word]++;
+                }
+                else
+                {
+                    wordsCount.Add(word, 0);
+                }
+            }
+
+            List<int> counts = wordsCount.Values.ToList();
+            counts.Sort();
+            counts.Reverse();
+
+
+            foreach (var i in counts.Take(10))
+            {
+                foreach (var pair in wordsCount)
+                {
+                    if (i == pair.Value)
+                    {
+                        Console.WriteLine($"{pair.Key} встречается в тексте {pair.Value} раз.");
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        static string[] GetText(string path)
+        {
+            string text;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            var words = noPunctuationText.Split(new char[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+    }
+}
